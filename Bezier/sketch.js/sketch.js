a680be3558468151ae5f4504c0f1290d0020f81b
Göksel Kınalı <carryvayne12211@gmail.com>
2021-10-05 00:02:12 +0300

    Added combination,fact and  calculate the square.

--- Bezier/sketch.js/sketch.js
+++ Bezier/sketch.js/sketch.js
@@ -5,6 +5,41 @@ var BezierScreenSizeY=800;
 var Points = [[]];
 
 
+function fact( sayi){
+  let fakt=1,i;
+  for(i=1;i<=sayi;i++){
+      fakt*=i;
+  }
+  return fakt;
+}
+
+
+
+function kombinasyon( sayi1, sayi2){
+  sayi1=sayi1-1;
+  let komb=fact(sayi1)/(fact(sayi1-sayi2)*fact(sayi2));
+
+if(sayi2==0)
+  return 1;
+
+  return komb;
+
+}
+
+
+function karealma( t, n, i){
+n=n-1;
+let a=pow(t,i);
+
+let b=pow((1-t),n-i);
+
+
+return a*b;
+
+}
+
+
+
 function setup() {
   createCanvas(BezierScreenSizeX,BezierScreenSizeY);
 }
@@ -28,14 +63,14 @@ function drawPoints(){
   i++;
   }
 }
-
+yeniX=[]
+yeniY=[]
 function SolveBezier(){
     for(var i=1;i<Points.length-3;i=i+2){
           for(var t=0;t<1;t=t+0.01){
 
-      yeniX=Points[i] * t  + Points[i+2] * (1-t );
-      yeniY=Points[i+1] * t  + Points[i+3] * (1-t );
-       ellipse(yeniX,yeniY,5);
+      yeniX.push(Points[i] * t  + Points[i+2] * (1-t ));
+      yeniY.push(Points[i+1] * t  + Points[i+3] * (1-t ));
 
           }
       
