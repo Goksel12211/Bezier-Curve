5f2f7064713eb54bee7ba7dff1a8afe4a7c3d4f7
Göksel Kınalı <carryvayne12211@gmail.com>
2021-10-06 01:25:31 +0300

    Fixed Bezier's calculate.(Index Problem).

--- Bezier/sketch.js/sketch.js
+++ Bezier/sketch.js/sketch.js
@@ -2,8 +2,76 @@
 var BezierScreenSizeX=1400;
 var BezierScreenSizeY=800;
 
-var Points = [[]];
+var PointX = [];
+var PointY = [];
 
+let sonucx=[];
+let sonucy=[];
+
+
+function setup() {
+  createCanvas(BezierScreenSizeX,BezierScreenSizeY);
+}
+
+
+function draw() {
+  drawPoints();
+  processBezier();
+ 
+for(let i =0;i<sonucx.length;i++){
+  noStroke();
+  fill(255);
+ellipse(sonucx[i],sonucy[i],20);
+}
+fill(0);
+
+}
+
+
+function mouseClicked(){
+PointX.push(mouseX);
+PointY.push(mouseY  );
+}
+
+
+function drawPoints(){
+    background(0);
+  fill(120,120,255);
+  for(var i=0;i<PointX.length;i++){
+  ellipse(PointX[i],PointY[i],20);
+  }
+}
+
+
+
+function processBezier(){
+
+var n = PointX.length
+
+for(let i=0;i<1001;i++){
+    sonucx[i]=0;
+    sonucy[i]=0;
+}
+
+  let a=0;
+  for(let t=0;t<1001;t=t+1){
+        a=t/1000.0;
+
+ for(let i=0;i<n;i++){
+        c=kombinasyon(n,i)  * PointX[i] ;
+        c=karealma(a,n,i)  * c ;
+                sonucx[t]=sonucx[t]+c;
+
+
+                b=kombinasyon(n,i)  * PointY[i] ;
+     b*=karealma(a,n,i);
+                sonucy[t]=sonucy[t]+b;
+
+  }
+
+}
+
+}
 
 function fact( sayi){
   let fakt=1,i;
@@ -38,44 +106,6 @@ return a*b;
 
 }
 
-
-
-function setup() {
-  createCanvas(BezierScreenSizeX,BezierScreenSizeY);
-}
-
-function draw() {
-  drawPoints();
-  SolveBezier();
-}
-
-
-function mouseClicked(){
-Points.push(mouseX,mouseY);
-}
-
-
-function drawPoints(){
-    background(0);
-  fill(120,120,255);
-  for(var i=1;i<Points.length-1;i++){
-  ellipse(Points[i],Points[i+1],20);
-  i++;
-  }
-}
-yeniX=[]
-yeniY=[]
-function SolveBezier(){
-    for(var i=1;i<Points.length-3;i=i+2){
-          for(var t=0;t<1;t=t+0.01){
-
-      yeniX.push(Points[i] * t  + Points[i+2] * (1-t ));
-      yeniY.push(Points[i+1] * t  + Points[i+3] * (1-t ));
-
-          }
-      
-    }
-}
 
 
 
